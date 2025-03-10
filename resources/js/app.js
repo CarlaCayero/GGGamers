c375618a4ab5bb4374ead3e94d5340d9b641bd05
Pablo Lin <162104715+LOSTYRL6@users.noreply.github.com>
2025-03-10 16:57:01 +0100

    VUE INSTALADO

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+import * as bootstrap from 'bootstrap';
+
+import { createApp } from 'vue';
+import PruebaVue from './componente/prueba.vue';
+
+// Crear la app con un objeto
+const app = createApp({});
+
+// Registrar el componente
+app.component('prueba', PruebaVue);
+
+// Montar Vue correctamente
+app.mount('#app');
