9f419599b35c7e913ef8308d06ecc3ae75d27227
Carla Cayero Hernández <ccayeroh2324@politecnics.barcelona>
2025-04-01 16:35:48 +0200

    mapbox terminado

--- resources/js/componente/mapa.vue
+++ resources/js/componente/mapa.vue
@@ -3,45 +3,49 @@
   </template>
 
   <script>
-  import mapboxgl from 'mapbox-gl'; // Importa Mapbox GL JS
+  import mapboxgl from 'mapbox-gl';
   import 'mapbox-gl/dist/mapbox-gl.css';
 
   export default {
     name: 'Mapa',
     mounted() {
-      // Clau d'accés de Mapbox
-      mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybGFjYXllcm8iLCJhIjoiY204eDZ3aWh1MDBnazJxcXgxeHl1dWg2aSJ9.EHGR1JHYwBI4fi9dwJCrJA';
+        // Token Mapbox
+        mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybGFjYXllcm8iLCJhIjoiY204eDZ3aWh1MDBnazJxcXgxeHl1dWg2aSJ9.EHGR1JHYwBI4fi9dwJCrJA';
 
-      // Inicialitza el mapa
-      const map = new mapboxgl.Map({
-        container: 'map', // ID del contenidor
-        style: 'mapbox://styles/mapbox/streets-v11', // Estil del mapa
-        center: [-0.126326, 51.533582], // Coordenades inicials
-        zoom: 15.27, // Zoom inicial
-        pitch: 42, // Inclinació
-        bearing: -50 // Rotació
-      });
+        // Init del mapa
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/carlacayero/cm8yl237n003q01qz6p8t3hmp',
+            center: [2.1731, 41.38877], // Coordenadas del mapa
+            zoom: 18,
+            bearing: -50,
+            hash: false,
+            interactive: true,
+            attributionControl: false
+        });
 
-      // Afegeix un marcador al mapa
-      new mapboxgl.Marker()
-        .setLngLat([-0.126326, 51.533582]) // Coordenades del marcador
-        .addTo(map);
+        // Marcador
+        new mapboxgl.Marker()
+            .setLngLat([2.1731, 41.38877]) // Coordenades del marcador
+            .addTo(map);
 
-      // Afegeix un control de navegació (zoom i rotació)
-      map.addControl(new mapboxgl.NavigationControl());
+        // control de navegación (zoom i rotación)
+        map.addControl(new mapboxgl.NavigationControl());
     }
   };
   </script>
 
-  <style>
+  <style scoped>
   body {
     margin: 0;
     padding: 0;
   }
   #map {
-    position: absolute;
+    position: relative;
     top: 0;
     bottom: 0;
     width: 100%;
+    height: 800px;
+    right: 10px;
   }
   </style>
