712b0cf22bdf50a19cda87294db94d2f5b9cd48a
Carla Cayero Hernández <ccayeroh2324@politecnics.barcelona>
2025-04-02 19:15:18 +0200

    card evento

---
+++ resources/js/componente/e-card.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="card">
+                <h5 class="card-header">TITULO</h5>
+                <div class="card-body">
+                    <h5 class="card-title">FECHA HORA</h5>
+                    <p class="card-text">descricion</p>
+                    <p class="card-text">Plataforma</p>
+                    <p class="card-text">Espacio</p>
+                    <p class="card-text">Participantes</p>
+                    <a href="#" class="btn btn-primary">Participar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.card {
+    background-color: #23023b;
+    color: white;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+}
+.card-header {
+    font-size: 18px;
+    font-weight: bold;
+    color: #c6ff41;
+}
+.card-body {
+    text-align: center;
+}
+.card-text{
+    font-family: "Exo 2";
+}
+.btn-primary {
+    background-color: #20f4ff;
+    color: #23023b;
+    padding: 10px 15px;
+    border-radius: 5px;
+    text-decoration: none;
+    transition: background 0.3s ease;
+}
+.btn-primary:hover {
+    background-color: #c6ff41;
+}
+</style>
